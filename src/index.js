3c521bbff413939dca3401e0560dca27b0a197ec
yoshiokamasahiro <34766492+yoshiokamasahiro@users.noreply.github.com>
2022-06-21 00:12:20 +0900

    テキストボックスを空にする処理

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const onClickAdd = () => {
   //テキストボックスの値を取得し、初期化する。
   const inputText = document.getElementById("add-text").value;
+  //テキストボックスを空にする。
   document.getElementById("add-text").value = "";
+  //テキストボックスに入力された値をアラートとして表示。
   alert(inputText);
 };
 
