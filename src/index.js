5ce5a761dfdba5d21a71b5ccec30e63cb3b110fb
yoshiokamasahiro <34766492+yoshiokamasahiro@users.noreply.github.com>
2022-06-27 21:50:20 +0900

    未完了リストにテキスト追加

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ const onClickAdd = () => {
   //テキストボックスを空にする。
   document.getElementById("add-text").value = "";
   //テキストボックスに入力された値をアラートとして表示。
-  alert(inputText);
+
+  //li生成
+  const liList = document.createElement("li");
+  liList.className = "list-row";
+
+  //li作成
+  const pText = document.createElement("p");
+  pText.innerText = inputText;
+
+  //ulタグの子要素に各要素を設定
+  liList.appendChild(pText);
+
+  //未完了のリストに追加
+  document.getElementById("incomplateList").appendChild(liList);
 };
 
 document
