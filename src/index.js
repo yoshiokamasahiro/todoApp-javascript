c86074eafce1fc7c15525f321c06d10ba8f04db0
yoshiokamasahiro <34766492+yoshiokamasahiro@users.noreply.github.com>
2022-07-06 22:02:08 +0900

    完成!

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ const onClickAdd = () => {
   //テキストボックスを空にする。
   document.getElementById("add-text").value = "";
   //テキストボックスに入力された値をアラートとして表示。
+  createIncomplateList(inputText);
+};
 
+//未完了リストから指定の要素を削除
+const delateFromIncomplateList = (target) => {
+  document.getElementById("incomplateList").removeChild(target);
+};
+
+//未完了汁とを追加する関数
+const createIncomplateList = (text) => {
   //li生成
   const liList = document.createElement("li");
   liList.className = "list-row";
-
   //ボタンエリア生成
   const btnArea = document.createElement("div");
   btnArea.classList = "btnArea";
-
   //pタグ作成(テキスト取得)
   const pText = document.createElement("p");
-  pText.innerText = inputText;
+  pText.innerText = text;
 
   //完了ボタン挿入
   const btnComplate = document.createElement("button");
@@ -25,10 +32,8 @@ const onClickAdd = () => {
     delateFromIncomplateList(btnRemove.closest(".list-row"));
     //完了リストに追加する要素
     const addTarget = btnComplate.closest(".list-row");
-
     //TODO内容テキストを取得
     const text = addTarget.firstElementChild.innerText;
-
     //li以下を初期化
     addTarget.textContent = null;
     //li以下に代入
@@ -39,6 +44,15 @@ const onClickAdd = () => {
 
     const backBtn = document.createElement("button");
     backBtn.innerText = "戻す";
+    backBtn.addEventListener("click", () => {
+      const removeTargetBack = backBtn.closest(".list-row");
+      document.getElementById("complateList").removeChild(removeTargetBack);
+
+      //テキストを取得
+      const text = removeTargetBack.firstElementChild.innerText;
+      //テキストを戻す
+      createIncomplateList(text);
+    });
 
     addTarget.appendChild(psec);
     addTarget.appendChild(divWrap);
@@ -65,11 +79,6 @@ const onClickAdd = () => {
 
   //未完了のリストに追加
   document.getElementById("incomplateList").appendChild(liList);
-};
-
-//未完了リストから指定の要素を削除
-const delateFromIncomplateList = (target) => {
-  document.getElementById("incomplateList").removeChild(target);
 };
 
 document
